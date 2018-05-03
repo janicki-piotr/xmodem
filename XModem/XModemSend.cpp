62a93fabc8f89ac526d7f334a3085441fe9ee7bf
PcPiotr <34127309+PcPiotr@users.noreply.github.com>
2018-05-03 19:01:26 +0200

    More code fixes

--- XModem/XModemSend.cpp
+++ XModem/XModemSend.cpp
@@ -14,41 +14,32 @@ const char ACK = 0x06;
 const char EOT = 0x04;
 const char C = 0x43;
 
-ifstream plik;
-char nazwaPliku[255];                   // bufor na nazwe pliku
 
-char znak;                              // bufor na przesylany znak
-int licznikZnakow = 1;
-unsigned long rozmiarZnaku = sizeof(znak);
-int kod;
 
-bool transmisja = false;
-bool czyPoprawnyPakiet;
-int nrPakietu = 1;
-char paczka[128];
+
+
+
+
+
+
+
 
 int Send(LPCTSTR selectedPort)
 {
-	HANDLE   portHandle;            // Handle for a port
-	DCB      controlSettings;       // Defines the control setting for a serial communications device.
-	COMSTAT	 commDeviceInfo;        // Contains information about a communications device. This structure is filled by the ClearCommError function
-	COMMTIMEOUTS timeParameters;	// Contains the time-out parameters for a communications device. 
-	DWORD    error;
-	USHORT tmpCRC;
-
-	portHandle = CreateFile(selectedPort, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	HANDLE portHandle = CreateFile(selectedPort, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 	if (portHandle != INVALID_HANDLE_VALUE)
 	{
+		DCB controlSettings;
 		controlSettings.DCBlength = sizeof(controlSettings);
 		GetCommState(portHandle, &controlSettings);
-		controlSettings.BaudRate = CBR_9600; 				// prêdkosæ transmisji
-		controlSettings.Parity = NOPARITY;   				// bez bitu parzystoœci
-		controlSettings.StopBits = ONESTOPBIT; 			// ustawienie bitu stopu (jeden bit)
-		controlSettings.ByteSize = 8;  					// liczba wysy³anych bitów
+		controlSettings.BaudRate = CBR_9600;
+		controlSettings.Parity = NOPARITY;
+		controlSettings.StopBits = ONESTOPBIT;
+		controlSettings.ByteSize = 8;
 
 		controlSettings.fParity = TRUE;
-		controlSettings.fDtrControl = DTR_CONTROL_DISABLE; //Kontrola linii DTR: DTR_CONTROL_DISABLE (sygna³ nieaktywny)
-		controlSettings.fRtsControl = RTS_CONTROL_DISABLE; //Kontrola linii RTR: DTR_CONTROL_DISABLE (sygna³ nieaktywny)
+		controlSettings.fDtrControl = DTR_CONTROL_DISABLE;
+		controlSettings.fRtsControl = RTS_CONTROL_DISABLE;
 		controlSettings.fOutxCtsFlow = FALSE;
 		controlSettings.fOutxDsrFlow = FALSE;
 		controlSettings.fDsrSensitivity = FALSE;
@@ -58,134 +49,148 @@ int Send(LPCTSTR selectedPort)
 		controlSettings.fErrorChar = FALSE;
 		controlSettings.fNull = FALSE;
 
+		COMMTIMEOUTS timeParameters;
 		timeParameters.ReadIntervalTimeout = 10000;
 		timeParameters.ReadTotalTimeoutMultiplier = 10000;
 		timeParameters.ReadTotalTimeoutConstant = 10000;
 		timeParameters.WriteTotalTimeoutMultiplier = 100;
 		timeParameters.WriteTotalTimeoutConstant = 100;
 
+		COMSTAT commDeviceInfo; DWORD error;
 		SetCommState(portHandle, &controlSettings);
 		SetCommTimeouts(portHandle, &timeParameters);
 		ClearCommError(portHandle, &error, &commDeviceInfo);
 	}
-	else {
+	else 
+	{
 		cout << "Conection failed" << endl;
+		system("PAUSE");
+		return (0);
 	}
 
 	cout << "File name: ";
-	cin >> nazwaPliku;
+	char fileName[255];
+	cin >> fileName;
 	cout << endl;
 
 	cout << "Waiting for transmittion." << endl;
+	char character; int characterCount = 1; unsigned long characterSize = sizeof(character);
+	int kod;
+	bool isTransmittion = false;
 	for (int i = 0; i < 6; i++)
 	{
 
-		ReadFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);
-		if (znak == 'C')
+		ReadFile(portHandle, &character, characterCount, &characterSize, NULL);
+		if (character == 'C')
 		{
 			kod = 1;
-			transmisja = true;
+			isTransmittion = true;
 			break;
 		}
-		else if (znak == NAK)
+		else if (character == NAK)
 		{
 			kod = 2;
-			transmisja = true;
+			isTransmittion = true;
 			break;
 		}
 	}
 
-	if (!transmisja) return(0);
+	if (!isTransmittion) return(0);
 
-	plik.open(nazwaPliku, ios::binary);
-	while (!plik.eof())
+	ifstream file;
+	file.open(fileName, ios::binary);
+
+	int packetNumber = 1;
+	char packet[128];
+	while (!file.eof())
 	{
 		for (int i = 0; i < 128; i++)
 		{
-			paczka[i] = (char)26;
+			packet[i] = (char)26;
 		}
 		int w = 0;
 
-		while (w < 128 && !plik.eof())
+		while (w < 128 && !file.eof())
 		{
-			paczka[w] = plik.get();
-			if (plik.eof()) paczka[w] = (char)26;
+			packet[w] = file.get();
+			if (file.eof()) packet[w] = (char)26;
 			w++;
 		}
-		czyPoprawnyPakiet = false;
+		bool isPacketCorrect = false;
 
-		while (!czyPoprawnyPakiet)
+		while (!isPacketCorrect)
 		{
 			cout << "Sending the packet" << endl;
-			WriteFile(portHandle, &SOH, licznikZnakow, &rozmiarZnaku, NULL);		// wysy³anie SOH
-			znak = (char)nrPakietu;
-			WriteFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);		// wys³anie numeru paczki
-			znak = (char)255 - nrPakietu;
-			WriteFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL); 		// wys³anie dope³nienia
+			WriteFile(portHandle, &SOH, characterCount, &characterSize, NULL);		// wysy³anie SOH
+			character = (char)packetNumber;
+			WriteFile(portHandle, &character, characterCount, &characterSize, NULL);		// wys³anie numeru paczki
+			character = (char)255 - packetNumber;
+			WriteFile(portHandle, &character, characterCount, &characterSize, NULL); 		// wys³anie dope³nienia
 
 
 			for (int i = 0; i < 128; i++)
 			{
-				WriteFile(portHandle, &paczka[i], licznikZnakow, &rozmiarZnaku, NULL);
+				WriteFile(portHandle, &packet[i], characterCount, &characterSize, NULL);
 			}
 			if (kod == 2) //suma kontrolna
 			{
-				char suma_kontrolna = 0;
+				char checksum = 0;
 				for (int i = 0; i < 128; i++)
 				{
-					suma_kontrolna += paczka[i] % 256;
+					checksum += packet[i] % 256;
 				}
-				WriteFile(portHandle, &suma_kontrolna, licznikZnakow, &rozmiarZnaku, NULL);
+				WriteFile(portHandle, &checksum, characterCount, &characterSize, NULL);
 			}
 			else if (kod == 1) //obliczanie CRC i transfer
 			{
-				tmpCRC = calculateCRC(paczka, 128);
-				znak = calculateCharacterCRC(tmpCRC, 1);
-				WriteFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);
-				znak = calculateCharacterCRC(tmpCRC, 2);
-				WriteFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);
+				USHORT tmpCRC = calculateCRC(packet, 128);
+				character = calculateCharacterCRC(tmpCRC, 1);
+				WriteFile(portHandle, &character, characterCount, &characterSize, NULL);
+				character = calculateCharacterCRC(tmpCRC, 2);
+				WriteFile(portHandle, &character, characterCount, &characterSize, NULL);
 			}
 
 			while (1)
 			{
-				znak = ' ';
-				ReadFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);
+				character = ' ';
+				ReadFile(portHandle, &character, characterCount, &characterSize, NULL);
 
-				if (znak == ACK)
+				if (character == ACK)
 				{
-					czyPoprawnyPakiet = true;
+					isPacketCorrect = true;
 					cout << "Packet transmitted successfully" << endl;
 					break;
 				}
-				if (znak == NAK)
+				if (character == NAK)
 				{
-					czyPoprawnyPakiet = true;
+					isPacketCorrect = true;
 					cout << "Checksum error" << endl;
 					break;
 				}
-				if (znak == CAN)
+				if (character == CAN)
 				{
 					cout << "Transmition failed" << endl;
 					return 1;
 				}
 			}
 		}
-		if (nrPakietu == 255) { nrPakietu = 1; }
-		else { nrPakietu++; }
+		if (packetNumber == 255) { packetNumber = 1; }
+		else { packetNumber++; }
 
 	}
-	plik.close();
+	file.close();
 
 	while (1)
 	{
-		znak = EOT;
-		WriteFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);
-		ReadFile(portHandle, &znak, licznikZnakow, &rozmiarZnaku, NULL);
-		if (znak == ACK) { break; }
+		character = EOT;
+		WriteFile(portHandle, &character, characterCount, &characterSize, NULL);
+		ReadFile(portHandle, &character, characterCount, &characterSize, NULL);
+		if (character == ACK) { break; }
 	}
 
 	CloseHandle(portHandle);
-	cout << "Hurra! Udalo sie wyslac plik!";
-	return 0;
+	cout << endl << "Transmission completed" << endl;
+	system("PAUSE");
+	return (0);
 
 }
